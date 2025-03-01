bef3872da35dca7d9e6f4a3a640aa5773a4224a2
lingzifreehost <a@lingzi.eu.org>
2025-03-02 02:37:43 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,42 @@
+var changeElements = [    
+    
+
+{name: "sitereptile", action: "remove"},  
+{name: "backup_wizard", action: "remove"},  
+{name: "ftp_connections", action: "remove"},
+{name: "remote_mysql", action: "remove"},
+{name: "Email_Accounts", action: "remove"},
+{name: "Email_Forwarders", action: "remove"},
+{name: "email_filters", action: "remove"},
+{name: "awstats", action: "remove"},
+{name: "errors", action: "remove"},
+{name: "php", action: "remove"},
+{name: "raw_access", action: "remove"},
+{name: "multiphp_configuration", action: "remove"},
+{name: "attracta_seotips", action: "remove"},
+{name: "lvephpsel", action: "remove"},
+ {name: "cron_jobs", action: "remove"},
+{name: "soft_div_icons", action: "remove"}, 
+{name: "cloudflare_performance", action: "remove"},   
+{name: "cloudflare_analytics", action: "remove"},
+
+{action: "remove", name: "accounts"},
+
+{name: "forwarders", action: "remove"},
+
+{name: "disk_usage", action: "remove"},
+{name: "ftp_accounts", action: "remove"},
+{name: "change_password", action: "remove"},
+{name: "change_language", action: "remove"},
+{name: "attracta_seotools", action: "remove"},
+
+{name: "createticket", action: "remove"},
+{name: "ShowTickets", action: "remove"},
+{name: "api_shell", action: "remove"},
+
+{name: "postgresql_databases", action: "remove"},    
+
+{name: "自定义域名绑定获取ssl方法", action: "add", group: "support", link: "https://note.youdao.com/s/ashGInTl",icon: "https://seekicon.com/free-icon-download/database-cloud_1.png",search_text: "domain"},
+{name: "文件上传及zip解压方法", action: "add", group: "support", link: "https://note.youdao.com/s/ashGInTl",icon: "https://seekicon.com/free-icon-download/database-cloud_1.png", search_text: "zip"}
+];
+document.write ("");
